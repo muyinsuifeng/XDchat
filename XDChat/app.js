704cb0677350564ffab38a22f87fd57293eee9ea
ming guo <ming guo>
2016-04-05 10:59:39 +0800

    mongodb

--- XDChat/app.js
+++ XDChat/app.js
@@ -5,6 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//采用connect-mongodb中间件作为Session存储  
+var session = require('express-session');  
+var Settings = require('./database/settings');  
+var MongoStore = require('connect-mongodb');  
+var db = require('./database/msession'); 
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -17,14 +25,38 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//session配置
+app.use(session({
+    cookie: { maxAge: 600000 },
+    secret: Settings.COOKIE_SECRET,
+    store: new MongoStore({  
+        username: Settings.USERNAME,
+        password: Settings.PASSWORD,
+        url: Settings.URL,
+        db: db})
+}))
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+// app.use('/', routes);
+// app.use('/users', users);
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', routes);
+app.use('/logout', routes);
+app.use('/home', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- XDChat/routes/index.js
+++ XDChat/routes/index.js
@@ -1,9 +1,64 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'XiaoDaiChat' });
+// });
+
+// module.exports = router;
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    res.render('index', { title: 'XiaoDaiChat' });
+});
+
+router.route('/login')
+.get(function(req, res) {
+    res.render('login', { title: 'login' });
+})
+.post(function(req, res) {
+    var user = {
+        username: 'admin',
+        password: '123456'
+    }
+    if(req.body.username === user.username && req.body.password === user.password){
+        req.session.user = user;
+        res.redirect('/home');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+router.get('/logout', function(req, res) {
+    req.session.user = null;
+    res.redirect('/');
+});
+
+router.get('/home', function(req, res) {
+    res.render('home', { title: 'Home' });
 });
 
 module.exports = router;
+
+// module.exports = function(app){
+// 	app.get('/', function(req, res){
+// 		res.sendfile('views/index.html');
+// 	});
+// 	app.get('/user', function(req, res){
+// 		var user = {
+//          	username: 'admin',
+//          	password: '123456'
+//      	}
+//      	var t =  req.body.username;
+//      	console.log(t);
+//     	if (req.body.username == user.username && req.body.password == user.password) {
+// 			res.sendfile('views/user.html');
+//      	} else {
+//          	res.sendfile('views/user.html');
+//      	}
+// 	});
+	
+// };
